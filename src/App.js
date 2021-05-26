a9fdafabee13366e1da922ece4ceebca41c6a95b
Madhav Bist <bistmadhav5@gmail.com>
2021-05-26 13:23:09 -0500

    Added article

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
   return (
     <div>
       <h1> Welcome to the git!!!!</h1>
+      <h2>This is second shot to conflict RK</h2>
+      <h3>I am working on MB BRANCH WITH</h3>
+      <article>
+        The national is the function we are the national is the function we can get 
+      </article>
     </div>
   )
 }
